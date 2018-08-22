e4c470f487c8c161848cb48b5f1e7f89f602e0e7
Andrey Loginov <loginovandreymob@gmail.com>
2018-08-22 15:20:13 +0300

    Fix3 in SetUser

--- src/GitBlocks.cpp
+++ src/GitBlocks.cpp
@@ -195,19 +195,15 @@ void GitBlocks::SetUser(wxCommandEvent &event)
 	SetUserDialog dialog(Manager::Get()->GetAppWindow());
 	if(dialog.ShowModal() == wxID_OK)
 	{
-	  if (wxMessageBox(_("Are you sure you want to Change Author name and email?"), _("Change Author name and email"), wxYES_NO) == wxYES)
+	  if (wxMessageBox(_("Are you sure you want to change Global Author name and email?"), _("Change Author name and email"), wxYES_NO) == wxYES)
 	  {
 
-		wxString command = git + _T(" config --global user.name '") + dialog.TextCtrl1->GetValue()+_T("'");
-		//wxMessageBox(command, _("Debug"), wxOK);
+		wxString command = git + _T(" config --global --replace-all user.name '") + dialog.TextCtrl1->GetValue()+_T("'");
 		ExecuteInTerminal(command, _("Set user name ..."), dialog.TextCtrl1->GetValue());
-        wxString command1 = git + _T(" config --global user.email '") + dialog.TextCtrl2->GetValue()+_T("'");
-      //wxMessageBox(command1, _("Debug"), wxOK);
+        wxString command1 = git + _T(" config --global --replace-all user.email '") + dialog.TextCtrl2->GetValue()+_T("'");
 		ExecuteInTerminal(command1, _("Set user email ..."), dialog.TextCtrl2->GetValue());
-
         wxString command2 = git + _T(" commit --amend --reset-author ")+ _T("Reset Author");
-        //	wxMessageBox(command2, _("Debug"), wxOK);
-		ExecuteInTerminal(command2, _("Reset Author ..."), _T(""));
+       	ExecuteInTerminal(command2, _("Reset Author ..."), _T(""));
 	  }
 
 	}
